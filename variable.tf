f448b59f94290fd6aa261c1e90e5c05c8a5867bf
Abhinav Sharma <abhinav32786@gmail.com>
2022-12-07 18:15:56 +0530

    My Commit

---
+++ variable.tf
@@ -0,0 +1,21 @@
+# variable "car"{
+#     # type= string
+#     type= list(string)
+  
+# }
+
+# variable "instance"{
+#     type= map(any)
+  
+# }
+v = {
+    "Name" = "Abhinav"
+    "Purpose" = "Testing"
+    "Owner" = "abhinav.sharma@cloudeq.com"
+  
+}
+
+variable "car"{
+    type= list(string)
+  
+}
